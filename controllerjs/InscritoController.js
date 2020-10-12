f81991e2329a77d8820d734656dea05685404fd7
kakalima76 <nieraldo@gmail.com>
2020-10-12 02:50:47 -0300

    v0.0.8

--- controllerjs/InscritoController.js
+++ controllerjs/InscritoController.js
@@ -1,6 +1,7 @@
 const { query } = require('express');
 const cnn = require('../db/sqlserver');
 const nodemailer = require("nodemailer");
+const { pool } = require('mssql');
 require('dotenv').config()
 const f = console.log;
 
@@ -17,6 +18,7 @@ module.exports.salvarInscrito = (req, res, next) => {
         })
     })
 
+    console.log(req.body);
 
     cnn.sql.connect(cnn.config)
         .then(pool => {
@@ -25,7 +27,7 @@ module.exports.salvarInscrito = (req, res, next) => {
                 .input('DataNascimento', cnn.sql.VarChar(20), req.body.DataNascimento)
                 .input('NickName', cnn.sql.VarChar(255), req.body.NickName)
                 .input('Email', cnn.sql.VarChar(255), req.body.Email)
-                .input('IDEquipe', cnn.sql.Int, req.body.IDEquipe)
+                .input('IDEquipe', cnn.sql.Int, parseInt(req.body.IDEquipe))
                 .input('CPF', cnn.sql.VarChar(25), req.body.CPF)
                 .input('CEP', cnn.sql.VarChar(25), req.body.CEP)
                 .query("insert into Inscritos(NomeInscrito, DataNascimento, NickName, Email, IDEquipe, CPF, CEP) values (@NomeInscrito, @DataNascimento, @NickName, @Email, @IDEquipe, @CPF, @CEP)");
@@ -79,7 +81,6 @@ module.exports.salvarEquipe = (req, res, next) => {
         })
     })
 
-
     cnn.sql.connect(cnn.config)
         .then(pool => {
             return pool.request()
@@ -89,11 +90,27 @@ module.exports.salvarEquipe = (req, res, next) => {
                 .query("insert into Equipes(IDTipoEquipe, NomeEquipe, CEP) values (@IDTipoEquipe, @NomeEquipe, @CEP)");
         })
         .then(result => {
-            sendJsonResponse(res, 200, result);
-            return cnn.sql.close();
+            cnn.sql.connect(cnn.config)
+                .then(pool => {
+                    return pool.request()
+                        .input('NomeEquipe', cnn.sql.VarChar(255), req.body.NomeEquipe)
+                        .query("select top (1) [IDEquipe] from Equipes where NomeEquipe = @NomeEquipe");
+                })
+                .then(data => {
+                    sendJsonResponse(res, 200, data.recordset[0].IDEquipe);
+                    return cnn.sql.close();
+                })
+                .catch(err => {
+                    sendJsonResponse(res, 404, err);
+                    console.log(error);
+                    return cnn.sql.close();
+                })
+            
+            // return cnn.sql.close();
         })
         .catch(err => {
             sendJsonResponse(res, 404, err);
+            console.log(err);
             return cnn.sql.close();
         })
 }
