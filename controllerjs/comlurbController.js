4400221c6f68a09fa3468ebbc02d4badc6324af0
kakalima76 <nieraldo@gmail.com>
2021-09-01 12:09:51 -0300

    autalização

--- controllerjs/comlurbController.js
+++ controllerjs/comlurbController.js
@@ -1,94 +1,131 @@
-const cnn = require('../db/sqlserver');
-require('dotenv').config()
+const cnn = require("../db/sqlserver");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 var sendJsonResponse = function (res, status, content) {
-    res.status(status);
-    res.json(content);
-}
+  res.status(status);
+  res.json(content);
+};
 
 module.exports.salvar = (req, res) => {
-    cnn.sql.on('error', () => {
-        sendJsonResponse(res, 401, {
-            msg: 'Falha na obtenção do recurso'
-        })
-    })
+  cnn.sql.on("error", () => {
+    sendJsonResponse(res, 401, {
+      msg: "Problemas de conexão",
+    });
+  });
 
-    cnn.sql.connect(cnn.config)
-        .then(pool => {
-             let result = pool.request()
-                .input('usuario', cnn.sql.VarChar(255), req.body.usuario)
-                .input('senha', cnn.sql.VarChar(255), req.body.senha)
-                .query("insert into login(usuario, senha) values (@usuario, @senha)");
+  bcrypt.hash(req.body.senha, 10, function (err, hash) {
+    if (err) {
+      sendJsonResponse(res, 401, {
+        msg: "Problemas com a encriptação",
+      });
+    }
 
-                return result;
-        })
-        .then(result => {
-            sendJsonResponse(res, 200, result);
-            return cnn.sql.close();
-        })
-        .catch(err => {
-            sendJsonResponse(res, 404, err);
-            console.log(err);
-            return cnn.sql.close();
-        })
-}
+    cnn.sql
+      .connect(cnn.config)
+      .then((pool) => {
+        let result = pool
+          .request()
+          .input("usuario", cnn.sql.VarChar(255), req.body.usuario)
+          .input("senha", cnn.sql.VarChar(255), hash)
+          .input("grupo", cnn.sql.Int, req.body.grupo)
+          .query(
+            "insert into login(usuario, senha, grupo) values (@usuario, @senha, @grupo)"
+          );
+
+        return result;
+      })
+      .then((result) => {
+        sendJsonResponse(res, 200, "Usuário salvo com sucesso");
+        return cnn.sql.close();
+      })
+      .catch((err) => {
+        sendJsonResponse(res, 404, err);
+        return cnn.sql.close();
+      });
+  });
+};
 
 module.exports.listar = (req, res) => {
-    let usuarios = [];
+  let usuarios = [];
 
+  cnn.sql.on("error", () => {
+    sendJsonResponse(res, 401, {
+      msg: "Falha na obtenção do recurso",
+    });
+  });
 
-    cnn.sql.on('error', () => {
-        sendJsonResponse(res, 401, {
-            msg: 'Falha na obtenção do recurso'
-        })
+  cnn.sql
+    .connect(cnn.config)
+    .then((pool) => {
+      let data = pool.request().query("select * from login");
+
+      return data;
     })
-
-
-    cnn.sql.connect(cnn.config)
-        .then(pool => {
-            let data = pool.request()
-                .query("select * from login");
-
-            return data;
-        })
-        .then(data => {
-            for (let i = 0; i < data.rowsAffected; i++) {
-                usuarios.push(data.recordset[i]);
-            }
-            sendJsonResponse(res, 200, usuarios);
-            return cnn.sql.close();
-        })
-        .catch(err => {
-            sendJsonResponse(res, 404, err);
-            return cnn.sql.close();
-        })
-}
+    .then((data) => {
+      for (let i = 0; i < data.rowsAffected; i++) {
+        usuarios.push(data.recordset[i]);
+      }
+      sendJsonResponse(res, 200, usuarios);
+      return cnn.sql.close();
+    })
+    .catch((err) => {
+      sendJsonResponse(res, 404, err);
+      return cnn.sql.close();
+    });
+};
 
 module.exports.getUsuario = (req, res) => {
+  cnn.sql.on("error", () => {
+    sendJsonResponse(res, 500, {
+      msg: "error 5",
+    });
+  });
 
-    cnn.sql.on('error', () => {
-        sendJsonResponse(res, 401, {
-            msg: 'Falha na obtenção do recurso'
-        })
+  cnn.sql
+    .connect(cnn.config)
+    .then((pool) => {
+      let data = pool
+        .request()
+        .input("usuario", cnn.sql.VarChar(255), req.body.usuario)
+        .query(
+          "select id, usuario, senha, grupo from login where usuario = @usuario"
+        );
+
+      return data;
     })
+    .then((data) => {
+      let tamanho = JSON.stringify(data).length;
+      if (data.recordset.length > 0) {
+        data = data.recordset[0];
+        bcrypt.compare(req.body.senha, data["senha"], function (err, result) {
+          if (err) {
+            sendJsonResponse(res, 401, {
+              msg: "error 1",
+            });
+            return cnn.sql.close();
+          } else {
+            if (result) {
+              var obj = {
+                id: data["id"],
+                usuario: data["usuario"],
+                grupo: data["grupo"],
+              };
 
-    cnn.sql.connect(cnn.config)
-        .then(pool => {
-            let data = pool.request()
-            .input('usuario', cnn.sql.VarChar(255), req.body.usuario)
-            .input('senha', cnn.sql.VarChar(255), req.body.senha)
-            .query("select id, usuario, senha from login where usuario = @usuario and senha = @senha");
-            
-            return data;
-        })
-        .then(data => {
-            
-            sendJsonResponse(res, 200, data.recordset[0]);
-            return cnn.sql.close();
-        })
-        .catch(err => {
-            sendJsonResponse(res, 404, err);
-            console.log(err);
-            return cnn.sql.close();
-        })
-}
+              sendJsonResponse(res, 200, obj);
+              return cnn.sql.close();
+            } else {
+              sendJsonResponse(res, 401, "error 2");
+              return cnn.sql.close();
+            }
+          }
+        });
+      } else {
+        sendJsonResponse(res, 401, "error 3");
+      }
+    })
+    .catch((err) => {
+      sendJsonResponse(res, 500, "error 4");
+      return cnn.sql.close();
+    });
+};
